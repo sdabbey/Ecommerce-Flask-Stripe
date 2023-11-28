c5f1be6ae10b7b122b4cfc978fecb8f1a9dc9675
quantiota <bouarfa.mahi@gmail.com>
2023-11-28 15:09:24 +0100

    update

--- app/views.py
+++ app/views.py
@@ -126,6 +126,7 @@ def create_checkout_session(path):
 # Product Index
 @app.route('/',          defaults={'path': 'pages/index.html'})
 @app.route('/products/', defaults={'path': 'products/index.html'})
+@app.route('/pages/', defaults={'path': 'pages/index.html'})
 def products_index(path):
 
     # Collect Products
