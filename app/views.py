bcfbf9979f3700711da6dc14d93689509770a91e
quantiota <bouarfa.mahi@gmail.com>
2023-11-30 12:20:37 +0100

    update

--- app/views.py
+++ app/views.py
@@ -122,9 +122,15 @@ def create_checkout_session(path):
         return jsonify({"sessionId": checkout_session["id"]})
     except Exception as e:
         return jsonify(error=str(e)), 403
+    
+
+    # Route for the home page
+@app.route('/',          defaults={'path': 'pages/index.html'})
+def home_index():
+    # You can add any logic here if needed for the home page
+    return render_template('pages/index.html')
 
 # Product Index
-@app.route('/',          defaults={'path': 'pages/index.html'})
 @app.route('/products/', defaults={'path': 'products/index.html'})
 def products_index(path):
 
@@ -146,7 +152,7 @@ def products_index(path):
             products.append( product )
 
     # Render Products Page
-    return render_template( 'pages/index.html', 
+    return render_template( 'ecommerce/index.html', 
                             products=products, 
                             featured_product=load_product_by_slug('featured') )
 
