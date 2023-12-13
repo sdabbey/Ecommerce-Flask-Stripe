97eb9f779969b8f8d6b3cb344c44a776fac44b67
quantiota <bouarfa.mahi@gmail.com>
2023-12-13 15:59:23 +0100

    update

--- app/views.py
+++ app/views.py
@@ -113,7 +113,8 @@ def create_checkout_session(path):
                     "currency": 'usd',
                     "amount": product.price * 100,
                 }
-            ]
+            ],
+       automatic_tax={"enabled": True}       
         )
         return jsonify({"sessionId": checkout_session["id"]})
     except Exception as e:
